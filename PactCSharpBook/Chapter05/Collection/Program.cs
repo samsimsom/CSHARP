8e7a379021eada42816ecc188384e338f1455995
sam sim som <samsimsom@gmail.com>
2021-02-08 09:01:39 +0300

    Test Commit

--- PactCSharpBook/Chapter05/Collection/Program.cs
+++ PactCSharpBook/Chapter05/Collection/Program.cs
@@ -7,6 +7,7 @@ namespace Collection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Mars!");
         }
     }
 }

e6f009cac0f14181b6aaa7a10b4a30f6267c37bb
samsimsom <samsimsom@gmail.com>
2021-02-08 09:36:57 +0300

    Collection

---
+++ PactCSharpBook/Chapter05/Collection/Person.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    public class Person : Object
+    {
+        // Fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+    }
+}
--- PactCSharpBook/Chapter05/Collection/Program.cs
+++ PactCSharpBook/Chapter05/Collection/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using System;
+using static System.Console;
 
 namespace Collection
 {
@@ -6,8 +8,22 @@ namespace Collection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Mars!");
+            var person1 = new Person();
+            person1.Name = "Bob";
+            person1.DateOfBirth = new DateTime(1984, 12, 15);
+            
+            person1.Children.Add(new Person { Name="Alfred" });
+            person1.Children.Add(new Person { Name="Jessica" });
+            
+            WriteLine($"{person1.Name} was born on " +
+                      $"{person1.DateOfBirth:dddd, d, MMMM, yyyy}");
+            WriteLine($"{person1.Name} has {person1.Children.Count} children.");
+            WriteLine($"{person1.Name}'s childrens names are :");
+            foreach (var children in person1.Children)
+            {
+                Write($"{children.Name} ");
+            }
+
         }
     }
 }

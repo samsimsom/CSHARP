c8b5b7e3d2a755c315359592eed2ce3c1ae0ebb6
sam sim som <samsimsom@gmail.com>
2021-02-09 11:06:54 +0300

    Random int[] Generator

--- Algorithms/Sorting/Sorting/Program.cs
+++ Algorithms/Sorting/Sorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using Helper;
 using static System.Console;
 
@@ -13,21 +14,26 @@ namespace Sorting
             helper.CleanScreen();
             WriteLine("-- Sorting Algorithms --");
             helper.LineSeperator();
-            
-            // TODO: Make it random int generator.
-            int[] data = new[]
-            {
-                1, 23, 44, 22, 76, 49, 85, 34, 2, 4, 8, 323, 68, 43, 795, 34,
-                35, 80, 67, 751, 57, 37, 96, 198, 5, 7, 36, 97, 63, 77, 59, 14
-            };
 
+            // Random generated int Array
+            int[] data = new int[10];
+            var rnd = new Random();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = rnd.Next(0, 500);
+            }
+
+            // Built-in Sorting Algorithm
+            var builtinShort = new BuiltIn();
+            builtinShort.BuiltInSort(data);
+            helper.LineSeperator();
+
+            // Bubble Sorting Algorithm
             var bubble = new Bubble();
             bubble.BubbleSort(data);
             helper.LineSeperator();
 
-            var builtinShort = new BuiltIn();
-            builtinShort.BuiltInSort(data);
-            helper.LineSeperator();
         }
     }
 }

6139d1c2cd3e1769b01a2c25a96ab18f57da32f6
samsimsom <samsimsom@gmail.com>
2021-02-12 20:13:59 +0300

    Chapter 06

---
+++ PactCSharpBook/Chapter06/PacktLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PacktLibrary
+{
+    public class Class1
+    {
+        
+    }
+}
---
+++ PactCSharpBook/Chapter06/People/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace People
+{
+    class Program
+    {
+        public static string Name { get; set; } = "Kitty";
+        
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine($"Hello {Name}");
+        }
+    }
+}

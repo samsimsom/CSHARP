acce89d9834906faf182499312b95d6f7b21bafe
sam sim som <samsimsom@gmail.com>
2021-02-13 21:19:45 +0300

    Chapter 06

--- PactCSharpBook/Chapter06/People/Program.cs
+++ PactCSharpBook/Chapter06/People/Program.cs
@@ -9,6 +9,26 @@ namespace People
         static void Main(string[] args)
         {
             WriteLine("Hello World!");
+
+            var harry = new Person() { Name = "Harry"};
+            var mary = new Person() { Name = "Mary" };
+            var jill = new Person() { Name = "Jill" };
+
+            // Call instance method
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Gary";
+
+            // Call static method
+            var baby2 = Person.Procreate(harry, jill);
+
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine(
+                format: "{0}'s first child is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name);
+            
         }
     }
 }

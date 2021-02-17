2dbbf1e7616b0a1be4e94944352f8417df9e4cf6
samsimsom <samsimsom@gmail.com>
2021-02-17 17:51:00 +0300

    Yellow Book

---
+++ YellowBook/YellowBook/WhileLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("--- While Loop ---");
+            WriteLine();            // Empty line
+            
+        }
+    }
+}

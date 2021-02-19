014d702036a206337005689224baa8cc7e733306
sam sim som <samsimsom@gmail.com>
2021-02-19 19:00:47 +0300

    Whack a Mole

---
+++ Games/WhackAMole/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using static System.Console;
+
+namespace WhackAMole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.Run();
+        }
+    }
+}

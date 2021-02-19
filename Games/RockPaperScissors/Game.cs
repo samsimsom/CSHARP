4a5e4dc0cde9f6355a476e25c500bda6e336a3af
samsimsom <samsimsom@gmail.com>
2021-02-19 09:21:25 +0300

    Games

---
+++ Games/RockPaperScissors/Game.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+using System.Globalization;
+using static System.Console;
+
+namespace RockPaperScissors
+{
+    public class Game
+    {
+        private enum Move { Rock, Paper, Scissors }
+        
+        // ---------------------------------------------------------------------
+        public void Run()
+        {
+            var random = new Random();
+            var wins = 0;
+            var losses = 0;
+
+            while (true)
+            {
+                Clear();
+                TitleDisplay();
+                
+                GetInput:
+                Write("Choose [r]ock, [p]aper, [s]cissors, or [e]xit : ");
+
+                Move playerMove;
+                switch (ReadLine().ToLower())
+                {
+                    case "rock" or "r": playerMove = Move.Rock; break;
+                    case "paper" or "p": playerMove = Move.Paper; break;
+                    case "scissors" or "s": playerMove = Move.Scissors; break;
+                    case "exit"or "e": Clear(); return;
+                    default: WriteLine("Invalid Input. Try Again..."); goto GetInput;
+                }
+
+                var computerMove = (Move) random.Next(3);
+                WriteLine($"The computer chose {computerMove}");
+                WriteLine($"Player move {playerMove}");
+                
+                
+            }
+            
+        }
+
+        // ---------------------------------------------------------------------
+        private void TitleDisplay()
+        {
+            WriteLine(new string('-', 35));
+            WriteLine("--- Rock - Paper - Scissors ---");
+            WriteLine(new string('-', 35));
+            WriteLine();
+        }
+        // ---------------------------------------------------------------------
+
+    }
+}
---
+++ Games/RockPaperScissors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            var game = new Game();
+            game.Run();
+            
+        }
+    }
+}

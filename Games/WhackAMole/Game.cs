5daa0ef34e253d60782bbae9768e99532ce9313f
samsimsom <samsimsom@gmail.com>
2021-02-20 21:06:48 +0300

    Games

--- Games/WhackAMole/Game.cs
+++ Games/WhackAMole/Game.cs
@@ -43,7 +43,61 @@ namespace WhackAMole
             // visual variables ------------------------------------------------
 
             var playTime = TimeSpan.FromSeconds(30);
+            SetConsoleSize();
+
+            while (true)
+            {
+                Clear();
+                WriteLine("Whack A Mole (Java Noob Edition)");
+                WriteLine();
+                WriteLine($"You have {(int)playTime.TotalSeconds} seconds to " +
+                          $"whack as many Java noobs as you can \nbefore the timer " +
+                          $"runs out. Use the number keys 1-9 to whack.\n" +
+                          $"Are you ready?");
+                WriteLine();
+                WriteLine("Play [Enter] or Quit [Esc]?");
+                
+                GetInput:
+                switch (ReadKey(true).Key)
+                {
+                    case ConsoleKey.Enter:
+                        Play(board, playTime);
+                        break;
+                    case ConsoleKey.Escape:
+                        Clear();
+                        WriteLine("Whack A Mole was closed...");
+                        Environment.Exit(0);
+                        break;
+                    default: goto GetInput;
+                }
+            }
+        }
+
+        private void Play(string board, TimeSpan playTime)
+        {
+            Clear();
+            WriteLine("Whack A Mole (Java Noob Edition)");
+            WriteLine();
+            WriteLine(board);
+            var start = DateTime.Now;
+            var score = 0;
+            var random = new Random();
+            var moleLocation = random.Next(1, 10);
+            CursorVisible = false;
+
+            while (DateTime.Now - start < playTime)
+            {
+            }
             
+        }
+        
+        private void SetConsoleSize()
+        {
+            if (OperatingSystem.IsWindows())
+            {
+                WindowWidth = Math.Max(WindowWidth, 50);
+                WindowHeight = Math.Max(WindowHeight, 22);
+            }
         }
     }
 }

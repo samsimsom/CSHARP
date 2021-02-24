5c6d21fe3d350dad6ee0c7ae7fa2ad7e20f75d21
sam sim som <samsimsom@gmail.com>
2021-02-24 11:05:01 +0300

    struct

--- PactCSharpBook/Chapter06/People/Program.cs
+++ PactCSharpBook/Chapter06/People/Program.cs
@@ -123,6 +123,20 @@ namespace People
             
             WriteLine(new string('-', 35));
             // -----------------------------------------------------------------
+            
+            // -----------------------------------------------------------------
+            WriteLine(new string('-', 35));
+
+            var dv1 = new DisplacementVector(3, 5);
+            var dv2 = new DisplacementVector(-2, 7);
+            var dv3 = dv1 + dv2;
+            
+            WriteLine($"({dv1.X}, {dv1.Y}) + ({dv2.X}, {dv2.Y}) = " +
+                      $"({dv3.X}, {dv3.Y})");
+            
+            WriteLine(new string('-', 35));
+            // -----------------------------------------------------------------
+
         }
 
         private static void Harry_Shout(object sender, EventArgs e)
